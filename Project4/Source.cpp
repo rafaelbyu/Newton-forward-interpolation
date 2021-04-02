d8802a7105435381b2687a84200b9c3bd95f493c
rafaelbyu <rafael.byu@gmail.com>
2021-04-02 22:48:53 +0300

    Окончательное изменение P.S. При интерполировании функции следите за тем как функция ведет себя на отрезке [a,b]!!

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -13,51 +13,32 @@ double f(double x)//задал функцию
 	return sqrt(sin(x * x));
 }
 
-//int factorial(int n) //факториал
-//{
-//	if (n < 0) // если введЄм отрицательное число
-//		return 0; // возвращаем ноль
-//
-//	if (n == 0) // если ввести 0
-//		return 1; // возвращаем факториал от нул€ = 1
-//
-//	else
-//		return n * factorial(n - 1);//вычисление факториала
-//}
-
 double NewtonOne(double a, double b, double x, double** delta, int n) // ѕерва€ »нтерпол€ционна€ формула Ќьютона
 {
 	double F = delta[0][1];
 	double h = (b - a) / n;											  //шаг интерпол€ции
 	double q = (x - a) / h;
 	double q1 = 1;
-
-	//for (int i = 0; i <= n; i++)
-	//{
-	//	F += (delta[n - i][i + 1] * q1) / factorial(n); //ф-ла n-ого члена интерпол€ционной формулы 
-
-	//	q1 = q1 * (q - i);
-	//}
-	//return F + f(x);
-
 	for (int i = 1; i <= n; i++)
-	{	
-		
-		q1 = q1 * (q - i + 1) / i;	
-
+	{			
+		q1 = q1 * (q - i + 1) / i;
 		F += delta[0][i + 1] * q1; 
 	}
 	return F;
 }
 
 void main()
-{
-	/*cout << "¬ведите x0 " << endl;*/
-	double a = -1;
-	/*cout << "¬ведите xn " << endl;*/
-	double b = 1;
+{	
+	setlocale(0, "");
+	
+	double a = 0;
+	double b = 0;
+	cout << "¬ведите x0 " << endl;
+	cin >> a;
+	cout << "¬ведите xn " << endl;
+	cin >> b;
 
-	setlocale(LC_ALL, "ru");
+	
 
 	ofstream fout;
 	fout.open("Interpol.txt");
@@ -91,13 +72,18 @@ void main()
 		for (int i = 0; i <= n; i++)		// 1 строка - значени€ функции f(x_i)
 		{
 			delta[i][1] = f(delta[i][0]);
+			//cout << delta[i][1] << " ";
 		}
+		/*cout << endl;*/
 		for (int m = 1; m <= n+1; m++)	// 2, 3, 4... и все последующие строки - это таблица конечных разностей
 		{
 			for (int i = 0; i <= n - m; i++)
 			{
 				delta[i][m + 1] = delta[i + 1][m] - delta[i][m];
+				//cout << setprecision(6) << delta[i][m + 1] << setw(6) << "\t";
 			}
+			/*cout << endl;
+			cout << endl;*/
 		}
 
 		//for (int i = 0; i <= n; i++)		// 1 строка - значени€ функции f(x_i)
